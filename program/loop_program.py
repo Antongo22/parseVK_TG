0f724c2f391a1862579cd159c2ddb7321618f843
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-04-13 00:34:47 +0300

    прописал структуру алгоритма

--- program/loop_program.py
+++ program/loop_program.py
@@ -3,3 +3,21 @@ class Program:
     #  Присоздании объекта будет небходимо указать сервис, кооторый мы парсим
     def __int__(self, service):
         self.service = service
+
+    def get_name(self):  # Получение названия группы
+        pass
+
+    def ceate_folder(self):  # Создание папки для скачивания
+        pass
+
+    def save_last_register_time(self):  # Запись в базу данных запись, с которолй началась выгрзка
+        pass
+
+    def save_meadia(self): # Сохранение всех данных в одку папку
+        pass
+
+    def end_program(self): # Условие и выход из программы
+        pass
+
+    def main(self): # Главный методработы программы
+        pass
