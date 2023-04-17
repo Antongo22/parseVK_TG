1527c8ad5ab4b89fec4b27718413ac4849ee7484
NickNameYouTuber <74864374+NickNameYouTuber@users.noreply.github.com>
2023-04-17 11:17:25 +0300

    обновил

--- vk/parser.py
+++ vk/parser.py
@@ -11,6 +11,8 @@ import urllib.request
 # response = requests.get(url)
 # print(response.text)
 
+
+wh = True
 new_name = ""
 last_posts = []
 count = 0
@@ -26,7 +28,7 @@ class Parser:
         browser.get('https://vk.com/feed')
 
         # Ждем 30 секунд
-        time.sleep(30)
+        time.sleep(1)
 
         browser.execute_script(f"window.open('{graf.grafic.reference}', '_self')")
         WebDriverWait(browser, 5).until(EC.presence_of_element_located(
@@ -34,31 +36,37 @@ class Parser:
         new_name = browser.find_element(By.XPATH, "//h1[@class='page_name']").text
 
     def download_images(self, browser, path):
-        global last_posts
-        from selenium.webdriver.common.action_chains import ActionChains
-        # Открываем вкладку с сайтом https://vk.com/...
-        time.sleep(3)
-
-        # WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH,
-        #                                                                 "//a[@class='wall_text']//img")))
-        posts = browser.find_elements(By.XPATH,
-                                      "//div[@class='MediaGridContainerWeb--post']//img")
-        print(posts)
-        posts2 = [i for i in posts if i not in last_posts]
-        last_posts += posts
-        posts = posts2
-
-        global count
-
-        for post in posts:
-            urllib.request.urlretrieve(str(post.get_attribute("src")), str(path) + f"/{str(count)}.jpg")
-            count += 1
-            actions = ActionChains(browser)
-            time.sleep(2)
-            browser.execute_script("window.scrollBy(0, 300)")
-            actions.move_to_element(post).perform()
+        try:
+            global last_posts
+            global wh
+            from selenium.webdriver.common.action_chains import ActionChains
+            # Открываем вкладку с сайтом https://vk.com/...
             time.sleep(3)
 
-        browser.execute_script("window.scrollBy(0, 2000)")
+            # WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH,
+            #                                                                 "//a[@class='wall_text']//img")))
+            posts = browser.find_elements(By.XPATH,
+                                          "//div[@class='MediaGridContainerWeb--post']//img")
+            print(posts)
+            print(wh)
+            posts2 = [i for i in posts if i not in last_posts]
+            last_posts += posts
+            posts = posts2
 
+            if posts == []:
+                raise Exception("Some exception")
 
+            global count
+
+            for post in posts:
+                urllib.request.urlretrieve(str(post.get_attribute("src")), str(path) + f"/{str(count)}.jpg")
+                count += 1
+                actions = ActionChains(browser)
+                time.sleep(2)
+                browser.execute_script("window.scrollBy(0, 300)")
+                actions.move_to_element(post).perform()
+                time.sleep(3)
+
+            browser.execute_script("window.scrollBy(0, 2000)")
+        except Exception:
+            wh = False
