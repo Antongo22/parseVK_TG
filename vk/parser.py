f79a87d771eb1f9937179c8dc4c3e243302916a1
NickNameYouTuber <74864374+NickNameYouTuber@users.noreply.github.com>
2023-04-21 12:51:26 +0300

    Комментарии

--- vk/parser.py
+++ vk/parser.py
@@ -1,3 +1,4 @@
+from selenium.common import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -35,10 +36,23 @@ class Parser:
         # Ждем 30 секунд
         time.sleep(30)
 
-        browser.execute_script(f"window.open('{graf.grafic.reference}', '_self')")
-        WebDriverWait(browser, 5).until(EC.presence_of_element_located(
-            (By.XPATH, "//h1[@class='page_name']")))
-        new_name = browser.find_element(By.XPATH, "//h1[@class='page_name']").text
+
+        try:
+            browser.execute_script(f"window.open('{graf.grafic.reference}', '_self')")
+            WebDriverWait(browser, 5).until(EC.presence_of_element_located(
+                (By.XPATH, "//h1[@class='page_name']")))
+            new_name = browser.find_element(By.XPATH, "//h1[@class='page_name']").text
+            print("ЖЖ " + new_name)
+            print("ф")
+        except NoSuchElementException:
+            if new_name is None:
+                print("as")
+                browser.execute_script(f"window.open('{graf.grafic.reference}', '_self')")
+                WebDriverWait(browser, 5).until(EC.presence_of_element_located(
+                    (By.XPATH, "//h2[@id='owner_page_name']")))
+                new_name = browser.find_element(By.XPATH, "//h2[@id='owner_page_name']").text
+                print(new_name)
+            # owner_page_name
 
     def download_images(self, browser, path):  # метод, скачивающий картинки
 
