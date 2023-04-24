d186281cd9fdc3e94848e009aa2921a5232cae8e
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-04-24 23:47:28 +0300

    доработал

--- facebook/parser.py
+++ facebook/parser.py
@@ -30,12 +30,16 @@ class Parser:
 
         time.sleep(50)  # Ждём 50 сек, т.к с впн медленее
 
-        browser.execute_script(f"window.open('{graf.grafic.reference}', '_self')")
+        try:
+            browser.execute_script(f"window.open('{graf.grafic.reference}', '_self')")
 
-        WebDriverWait(browser, 5).until(EC.presence_of_element_located(
-            (By.XPATH, "//h1[@class='x1heor9g x1qlqyl8 x1pd3egz x1a2a7pz']")))
-        new_name = browser.find_element(By.XPATH, "//h1[@class='x1heor9g x1qlqyl8 x1pd3egz x1a2a7pz']").text
-        print(new_name)
+            WebDriverWait(browser, 5).until(EC.presence_of_element_located(
+                (By.XPATH, "//div[@class='x78zum5 xdt5ytf x1wsgfga x9otpla']")))
+            new_name = browser.find_element(By.XPATH, "//div[@class='x78zum5 xdt5ytf x1wsgfga x9otpla']").text
+            print(new_name)
+        except:
+            pass
+
         if new_name == "":
             new_name = "Неопределённая страница в facebook"
 
@@ -48,7 +52,9 @@ class Parser:
 
         # Загружаем посты с изображениями
         posts = browser.find_elements(By.XPATH,
-                                      "//a[@class='x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x1a2a7pz x1lliihq x1pdlv7q']//img")
+                                      "//a [@class='x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x1a2a7pz x1lliihq x1pdlv7q']//img")
+        sec_posts = browser.find_elements(By.XPATH, "//div[@class = 'x6ikm8r x10wlt62 x10l6tqk']//img")
+        posts += sec_posts
         print(posts)
 
         # Отсоединение уже скачанных постов от не
