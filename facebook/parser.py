fe699cf6615d9320c1fc603d12239c8c3b189a40
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-04-26 18:40:52 +0300

    фэйсбук нормално работает

--- facebook/parser.py
+++ facebook/parser.py
@@ -67,7 +67,11 @@ class Parser:
                                       "//a [@class='x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x1a2a7pz x1lliihq x1pdlv7q']//img")
 
         sec_posts = browser.find_elements(By.XPATH, "//div[@class = 'x6ikm8r x10wlt62 x10l6tqk']//img")
-        posts += sec_posts
+
+        for i in sec_posts:
+            if i not in posts:
+                posts.append(i)
+
         print(posts)
 
         # Отсоединение уже скачанных постов от не
@@ -113,16 +117,26 @@ class Parser:
         WebDriverWait(browser, 20).until(EC.presence_of_element_located(
             (By.XPATH, "//div[@class='xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs x126k92a']")))
 
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located(
+            (By.XPATH, "//div[@class='xzsf02u xngnso2 xo1l8bm x1qb5hxa']")))
 
         # Загружаем посты с изображениями
         posts = browser.find_elements(By.XPATH,
                                       "//div[@class='x1iorvi4 x1pi30zi x1l90r2v x1swvt13']")
 
         sec_posts = browser.find_elements(By.XPATH,
-                                      "//div[@class='xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs x126k92a']")
+                                          "//div[@class='xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs x126k92a']")
 
         fir_posts = browser.find_elements(By.XPATH,
-                                          "//div[@class='xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs']")
+                                          "//div[@class='xzsf02u xngnso2 xo1l8bm x1qb5hxa']")
+
+        for i in sec_posts:
+            if i not in posts:
+                posts.append(i)
+
+        for i in fir_posts:
+            if i not in posts:
+                posts.append(i)
 
         posts += sec_posts
         posts += fir_posts
@@ -142,12 +156,13 @@ class Parser:
         # Прохождение по картинкам и их скачивание
         for post in posts:
 
-
             try:
                 WebDriverWait(browser, 20).until(EC.presence_of_element_located(
-                    (By.XPATH, "//div[@class='x11i5rnm xat24cr x1mh8g0r x1vvkbs xtlvy1s x126k92a']//div[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f']")))
+                    (By.XPATH,
+                     "//div[@class='x11i5rnm xat24cr x1mh8g0r x1vvkbs xtlvy1s x126k92a']//div[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f']")))
 
-                mor = post.find_element(By.XPATH, "//div[@class='x11i5rnm xat24cr x1mh8g0r x1vvkbs xtlvy1s x126k92a']//div[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f']")
+                mor = post.find_element(By.XPATH,
+                                        "//div[@class='x11i5rnm xat24cr x1mh8g0r x1vvkbs xtlvy1s x126k92a']//div[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f']")
                 # Скачивание картинки
                 mor.click()
 
